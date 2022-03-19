0bac85fa094a926c80f6f138574b088fd710ce21
Saikim <kim_romeu@hotmail.com>
2022-03-19 10:57:25 -0300

    add ListaOvos e mod no router

--- projeto_pascoa/src/router/index.js
+++ projeto_pascoa/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ListaOvos from '../views/ListaOvos'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/ovos',
+    name: 'ListaOvos',
+    component: ListaOvos
+  }
 ]
 
 const router = new VueRouter({
---
+++ projeto_pascoa/src/views/ListaOvos.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-container>
+        <h2>Teste</h2>
+
+        <v-simple-table>
+            <template v-slot:default>
+            <thead>
+                <tr>
+                <th class="text-left">
+                    Name
+                </th>
+                <th class="text-left">
+                    Calories
+                </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                v-for="item in desserts"
+                :key="item.name"
+                >
+                <td>{{ item.name }}</td>
+                <td>{{ item.calories }}</td>
+                </tr>
+            </tbody>
+            </template>
+  </v-simple-table>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name:"ListaOvos"
+}
+</script>
+
+<style scoped>
+
+</style>
